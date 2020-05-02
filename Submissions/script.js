cf47121c7d0858fd83fe729102686171c4e93717
mandarizka <64694325+mandarizka@users.noreply.github.com>
2020-05-02 21:18:55 +0700

    Add files via upload

---
+++ Submissions/script.js
@@ -0,0 +1,9 @@
+console.log("Selamat Anda berhasil menggunakan JavaScript pada Website")
+alert("Selamat Membaca!!");
+
+const first = document.getElementById('first');
+first.innerHTML = 'Kota yang dingin namun penuh dengan kehangatan.';
+
+const jumbotron = document.querySelector('.jumbotron');
+jumbotron.style.backgroundColor = '#1E9E11';
+
